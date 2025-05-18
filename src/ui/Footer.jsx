d5df462b8027c740a317da58f517f2174f3179bd
ranasauravsingh <ranasaurav121@gmail.com>
2025-05-18 19:35:39 +0530

    [Design][Footer] integrated footer design

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+    const currentYear = new Date().getFullYear();
+	return (
+		<footer className="bg-gray-200 py-4 sm:py-6">
+			<div className="container mx-auto text-center">
+				<p className="text-sm sm:text-base text-gray-600">
+					Crafted with Code & Soul by <span className="inline-block text-coral-900 bg-gradient-bg bg-clip-text text-transparent">ranasauravsingh</span> &copy; {currentYear}.
+				</p>
+			</div>
+		</footer>
+	);
+};
 
-export default Footer
+export default Footer;
