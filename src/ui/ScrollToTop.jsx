bbcabfa3e74ac429821cac017dadf28a97cec552
ranasauravsingh <ranasaurav121@gmail.com>
2025-05-20 06:38:24 +0530

    [Refactor][UI] updated height width for max width sm mobile sizes

--- src/ui/ScrollToTop.jsx
+++ src/ui/ScrollToTop.jsx
@@ -30,7 +30,7 @@ const ScrollToTop = () => {
 	return (
 		<MotionButton
 			onClick={scrollToTop}
-			className={`fixed cursor-pointer bottom-4 right-4 sm:bottom-6 sm:right-6 bg-gradient-bg w-10 h-10 sm:w-12 sm:h-12 flex items-center justify-center rounded-full shadow-lg hover:shadow-xl transition-all duration-300 ${
+			className={`fixed cursor-pointer bottom-4 right-4 sm:bottom-6 sm:right-6 bg-gradient-bg w-12 h-12 sm:w-12 sm:h-12 flex items-center justify-center rounded-full shadow-lg hover:shadow-xl transition-all duration-300 ${
 				isVisible ? "opacity-100" : "opacity-0"
 			}`}
 			initial={{ scale: 0 }}
@@ -38,7 +38,7 @@ const ScrollToTop = () => {
 			transition={{ duration: 0.3 }}
 			aria-label="Scroll to top"
 		>
-			<ArrowUpIcon className="h-5 w-5 sm:h-6 sm:w-6 text-white" />
+			<ArrowUpIcon className="h-6 w-6 sm:h-6 sm:w-6 text-white" />
 		</MotionButton>
 	);
 };
